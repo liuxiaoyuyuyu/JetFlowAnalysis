cb2d31d138f854a1df02ec808a09b88cf76f318f
Xiaoyu Liu <xl155@rice.edu>
2024-08-15 22:05:12 +0200

    script for submitting condor jobs

--- JetTreeAnalyzer/batch/data_vn500_test.sh
+++ JetTreeAnalyzer/batch/data_vn500_test.sh
@@ -6,7 +6,12 @@ cd ~xiaoyul/MYDEMOANALYZER/CMSSW_13_3_0/src/
 
 #cmsenv
 eval `scramv1 runtime -sh`
+#cmssw-el7
 cd ~xiaoyul/MYDEMOANALYZER/CMSSW_13_3_0/src/JetFlowAnalysis/JetTreeAnalyzer/batch
 echo PWD: $PWD
 echo $HOSTNAME
+#../bin/new_default_data_1d2d.exe ./run3_data/list_25/list_job$1 0 1
+#../bin/data_QA.exe ./run3_data/list_25/list_job$1 0 1
+#../bin/data_QA.exe ./run3_data/list_25_hlt260/list_job$1 0 1
+#../bin/data_statistics.exe ./run3_data/list_25/list_job$1 0 1
 ../bin/new_default_data_vn.exe ./Run3_tree_list/2023/list_25/list_job$1 0 1
