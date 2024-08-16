59db883352f016369f2708d80923ce6594c7c400
liuxiaoyuyuyu <xl155@rice.edu>
2024-08-16 16:04:18 -0500

    Update Draw2DCorr.C

--- Macros/Draw2DCorr.C
+++ Macros/Draw2DCorr.C
@@ -2,12 +2,18 @@ void Draw2DCorr(){
     TH2D* hSignal[5][2];//Ntrk,pt
     TH2D* hBkg[5][2];
     TH2D* h2DCorr[5][2];
-    TFile* f= new TFile("/Users/xl155/Documents/JetFlow_Run3_data/job_round3_run3.root","READ");
+    TFile* f= new TFile("/Users/xl155/Documents/JetFlow_Run3_data/ana_run3.root","READ");
+    //TFile* f= new TFile("/Users/xl155/Documents/JetFlow_Run3_data/new_default_complete_vn.root","READ");
     int   trackbinbounds[5]= {76,78,80,81,82};
     int ptbinbounds[2]={3,5};
+    //int   trackbinbounds[5]= {83,78,80,81,82};
+    //int ptbinbounds[2]={3,5};
     TH1D* hJetPass = (TH1D*)f->Get("hJet_Pass550");
+    //TH1D* hJetPass = (TH1D*)f->Get("hJet_Pass550_hltCor");
     for(int i=0;i<5;i++){
         for(int j=0;j<2;j++){
+    //for(int i=0;i<1;i++){
+        //for(int j=0;j<1;j++){
             hSignal[i][j]=(TH2D*)f->Get(Form("hSigS_Cor_%d_to_1000_and_%d_to_30_w_PU_1",trackbinbounds[i],ptbinbounds[j]));
             hBkg[i][j]=(TH2D*)f->Get(Form("hBckS_Cor_%d_to_1000_and_%d_to_30_w_PU_1",trackbinbounds[i],ptbinbounds[j]));
             h2DCorr[i][j]=(TH2D*)hSignal[i][j]->Clone(Form("h2DCorr_Cor_%d_to_1000_and_%d_to_30_w_PU_1",trackbinbounds[i],ptbinbounds[j]));
@@ -17,13 +23,16 @@ void Draw2DCorr(){
             for(int ieta=0;ieta<h2DCorr[i][j]->GetNbinsX();ieta++){
                 for(int iphi=0;iphi<h2DCorr[i][j]->GetNbinsY();iphi++){
                     double tmp_njet=hJetPass->GetBinContent(i+1);
+                    //double tmp_njet=hJetPass->GetBinContent(9);
                     double tmp_sig=hSignal[i][j]->GetBinContent(ieta+1,iphi+1);
                     double tmp_bkg=hBkg[i][j]->GetBinContent(ieta+1,iphi+1);
                     double tmp_b00=hBkg[i][j]->GetBinContent(hBkg[i][j]->FindBin(0,0));
-                    double tmp_bin=tmp_b00*tmp_sig/(tmp_bkg*tmp_njet);
-                    //double tmp_bin=tmp_b00*tmp_sig/(tmp_bkg*tmp_njet*x_bw*y_bw);
+                    //double tmp_bin=tmp_b00*tmp_sig/(tmp_bkg*tmp_njet);
+                    double tmp_bin=tmp_b00*tmp_sig/(tmp_bkg*tmp_njet*x_bw*y_bw);
+                    //double tmp_bin=tmp_b00*tmp_sig/(tmp_bkg*tmp_njet*x_bw);
                     //cout<<tmp_bin<<endl;
-                    if(std::isnan(tmp_bin)) continue;
+                    //if(std::isnan(tmp_bin)) continue;
+                    if(!std::isfinite(tmp_bin)) continue;
                     h2DCorr[i][j]->SetBinContent(ieta+1,iphi+1,tmp_bin); 
                 }
             }
@@ -31,10 +40,13 @@ void Draw2DCorr(){
         }
     }
     
-    TFile* fout= new TFile("/Users/xl155/Documents/JetFlow_Run3_data/2DCorr_run3_round3_nobw.root","RECREATE");
+    //TFile* fout= new TFile("/Users/xl155/Documents/JetFlow_Run3_data/2DCorr_run2_PG_ana_eta_phi_bw.root","RECREATE");
+    TFile* fout= new TFile("/Users/xl155/Documents/JetFlow_Run3_data/2DCorr_run3.root","RECREATE");
     
     for(int i=0;i<5;i++){
         for(int j=0;j<2;j++){
+    //for(int i=0;i<1;i++){
+        //for(int j=0;j<1;j++){
             h2DCorr[i][j]->Write();
         }
     }
