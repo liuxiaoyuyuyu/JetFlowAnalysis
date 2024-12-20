335872117ae2e752b21fe744305b41aa707e16bd
Xiaoyu Liu <xl155@rice.edu>
2024-12-20 15:25:39 -0600

    Update new_default_mc_vn.C

--- JetTreeAnalyzer/src/new_default_mc_vn.C
+++ JetTreeAnalyzer/src/new_default_mc_vn.C
@@ -149,7 +149,7 @@ void MyClass::Loop(int job, std::string fList){
     for(int f = 0; f<fileList.size(); f++){
         int f_from_file = f;
         fFile = TFile::Open(fileList.at(f).c_str(),"read");
-        TTree *tree = (TTree*)fFile->Get("analyzerOffline/trackTree");
+        TTree *tree = (TTree*)fFile->Get("trackTree");
         Init(tree);
 
         std::cout << "File " << f+1 << " out of " << fileList.size() << std::endl;
